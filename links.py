9b2c5a80d8b39e86a6fe1d61097dbff37868e5a5
mintydevdaz <mintydevdaz@proton.me>
2022-12-30 16:18:58 +1100

    4.0 added new url, created stations dict

---
+++ links.py
@@ -0,0 +1,14 @@
+urls = [
+    'https://api.onegov.nsw.gov.au/FuelCheckApp/v1/fuel/prices/bylocation?bottomLeftLatitude=-33.757604620057066&bottomLeftLongitude=151.12944436975098&topRightLatitude=-33.68564429892917&topRightLongitude=151.29406763024903&fueltype=E10-U91&brands=SelectAll',
+    'https://api.onegov.nsw.gov.au/FuelCheckApp/v1/fuel/prices/bylocation?bottomLeftLatitude=-33.781298954307054&bottomLeftLongitude=151.0914061604004&topRightLatitude=-33.72120999561731&topRightLongitude=151.35061483959961&fueltype=E10-U91&brands=BP',
+    'https://api.onegov.nsw.gov.au/FuelCheckApp/v1/fuel/prices/bylocation?bottomLeftLatitude=-33.81572472318192&bottomLeftLongitude=151.09231860021976&topRightLatitude=-33.77777893294506&topRightLongitude=151.1745443997803&fueltype=E10-U91&brands=Ampol',
+    'https://api.onegov.nsw.gov.au/FuelCheckApp/v1/fuel/prices/bylocation?bottomLeftLatitude=-33.90313988829917&bottomLeftLongitude=151.14515755010984&topRightLatitude=-33.88418850057846&topRightLongitude=151.18627044989012&fueltype=E10-U91&brands=7-Eleven'
+]
+
+stations = {
+    'EG Ampol Glenrose': 643,
+    'Ampol Belrose': 18424,
+    'BP Forestville': 410,
+    'Ampol Woolies North Ryde': 834,
+    '7-Eleven Camperdown': 17119
+}
