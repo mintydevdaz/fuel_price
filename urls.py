0f6588ad7c5809d5dca727484caac0cd95977656
mintydevdaz <mintydevdaz@proton.me>
2022-12-30 15:45:24 +1100

    API calls with station fuel prices

---
+++ urls.py
@@ -0,0 +1,4 @@
+urls = [
+    'https://api.onegov.nsw.gov.au/FuelCheckApp/v1/fuel/prices/bylocation?bottomLeftLatitude=-33.757604620057066&bottomLeftLongitude=151.12944436975098&topRightLatitude=-33.68564429892917&topRightLongitude=151.29406763024903&fueltype=E10-U91&brands=SelectAll',
+    'https://api.onegov.nsw.gov.au/FuelCheckApp/v1/fuel/prices/bylocation?bottomLeftLatitude=-33.781298954307054&bottomLeftLongitude=151.0914061604004&topRightLatitude=-33.72120999561731&topRightLongitude=151.35061483959961&fueltype=E10-U91&brands=BP'
+    ]
