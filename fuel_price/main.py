03f13d63602e3512dc219ab9ba51d812e2e473e7
mintydevdaz <mintydevdaz@proton.me>
2023-01-23 10:08:58 +1100

    5.2. minor changes

--- fuel_price/main.py
+++ fuel_price/main.py
@@ -1,61 +1,61 @@
+import sys
+
 import pandas as pd
 import requests
-
-from links import urls, stations
+from links import stations, urls
 
 
 def main():
     items = []
-    # Get JSON from multiple URLs
     for url in urls:
 
-        # Request HTML
-        r = get_request(url)
-
-        # Get JSON
-        json = get_json(response=r)
+        # Get HTML and JSON response
+        r = get_html_response(url)
+        json = get_json(r)
 
         # Append if JSON returns single station
         if len(json) == 1:
             items.append(json[0])
         else:
             # Append specific stations if JSON contains multiple stations
-            items.extend(j for j in json if j.get('ServiceStationID') in stations.values())  # noqa
+            items.extend(
+                j for j in json if j.get("ServiceStationID") in stations.values()
+            )
 
     # Extracts prices and fuel types from JSON
     for item in items:
         prices = clean(current_prices(item))
-        print(location(item))
+        print(station_address(item))
         print(table(prices))
-        print('-'*40)
+        print("-" * 40)
 
 
-def get_request(url: str):
-    '''Get request from Fuel Check NSW API'''
+def get_html_response(url: str) -> requests.models.Response:
     try:
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}  # noqa
-        r = requests.get(url, headers=headers, timeout=6)
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+        }
+        r = requests.get(url, headers=headers, timeout=5)
         r.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        print(f'Error loading webpage: {e}')
-    except requests.exceptions.Timeout as e:
-        print(f'Request Timeout: {e}')
+    except requests.exceptions.HTTPError as http_err:
+        sys.exit(f"HTTP error occurred: \n'{http_err}'")
+    except requests.exceptions.Timeout as timeout_err:
+        sys.exit(f"Timeout error occurred: \n'{timeout_err}'")
+    except Exception as err:
+        sys.exit(f"Error occurred: \n'{err}'")
     return r
 
 
 def get_json(response: requests.models.Response) -> list:
-    '''Parse JSON'''
     try:
         r = response.json()
-    except requests.exceptions.JSONDecodeError as e:
-        print(f'Unable to grab JSON file: {e}')
+    except requests.exceptions.JSONDecodeError as err:
+        sys.exit(f"Unable to grab JSON file: \n'{err}'")
     return r
 
 
 def current_prices(json: dict) -> list[str]:
-    '''Get fuel prices from JSON'''
-    prices = json['Prices']
+    prices = json["Prices"]
     r = []
     for price in prices:
         r.extend(iter(price.values()))
@@ -63,21 +63,21 @@ def current_prices(json: dict) -> list[str]:
 
 
 def clean(data: list[str]) -> list:
-    '''Joins fuel type with it's current price'''
+    """Joins fuel type with it's current price"""
     fuel_type = data[::4]
     price = data[1::4]
     return list(zip(fuel_type, price))
 
 
-def location(json: dict) -> str:
-    '''Show address of fuel station'''
-    return f"{json['Name']} | {json['Address']} | ID: {json['ServiceStationID']}"  # noqa
+def station_address(json: dict) -> str:
+    return (
+        f"ID: {json['ServiceStationID']}\n{json['Name']} | {json['Address']}"  # noqa
+    )
 
 
 def table(prices: list) -> pd.DataFrame:
-    '''Convert fuel types and prices into a Pandas Dataframe'''
-    return pd.DataFrame(prices, columns=['Fuel Type', 'Price/L'])
+    return pd.DataFrame(prices, columns=["Fuel Type", "Price/L"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
